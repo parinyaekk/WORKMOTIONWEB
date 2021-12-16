e2e627502bdc6e9a66660cbdc10dd42225568249
parinya.ekk@gmail.com <parinya.ekk@gmail.com>
2021-12-16 13:18:15 +0700

    -Contact Form -Endpoint Front-End

--- src/MainConfig.js
+++ src/MainConfig.js
@@ -1,6 +1,6 @@
 module.exports = global.config = {
     variable: {        
-        Url: "https://localhost:44373/api/", // Localhost
+        Url: "https://localhost:44373/API/", // Localhost
         ImagePath: "https://localhost:44373/", // PathImageServer Localhost
         Google_Key: "AIzaSyBSt60GOJz7Qt3RM4YbBjpnfOTL64VWMXI", // Google_Key Dev
     }
