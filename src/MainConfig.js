68396bee4aa1f322b12797fa7a8d17c5c72fe8a8
parinya.ekk@gmail.com <parinya.ekk@gmail.com>
2022-01-07 12:38:06 +0700

    stg

--- src/MainConfig.js
+++ src/MainConfig.js
@@ -2,7 +2,7 @@ module.exports = global.config = {
     variable: {        
         // Url: "https://localhost:44373/API/", // Localhost
         // ImagePath: "https://localhost:44373/", // PathImageServer Localhost
-        Url: "topventure-api.myip.in.th/API/", // STG
-        ImagePath: "topventure-api.myip.in.th/", // PathImageServer STG
+        Url: "https://topventure-api.myip.in.th/API/", // STG
+        ImagePath: "https://topventure-api.myip.in.th/", // PathImageServer STG
     }
 };
